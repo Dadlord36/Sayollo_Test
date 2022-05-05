e396614e6d49a5e2efcb41ebb2d67153f0cf7773
Roman Shkirenko <dadlord36@gmail.com>
2022-05-05 13:40:31 +0300

    Purchased item loading is optimized now.

--- Assets/Sources/ViewModels/ItemsViewModel.cs
+++ Assets/Sources/ViewModels/ItemsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Models.JSON;
 using Repositories.Interfaces;
 using REST_API.Interfaces;
@@ -12,33 +13,31 @@ namespace ViewModels
     {
         private readonly IItemsRepository _itemsRepository;
         private readonly ItemView _itemView;
-        [Inject]
-        private I_API_Provider _provider;
+        [Inject] private I_API_Provider _provider;
+        private readonly TaskFactory mainThreadTaskFactory;
 
         public ItemsViewModel(IItemsRepository itemsRepository, ItemsPageView itemsPageView, ItemView itemView)
         {
             _itemsRepository = itemsRepository;
             _itemView = itemView;
             itemsPageView.ItemPurchaseRequested += PurchaseAnItem;
+            mainThreadTaskFactory = new TaskFactory(TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private async void PurchaseAnItem()
         {
             try
             {
-                ProductResponseModel itemData = await _itemsRepository.GetPresentItem();
+                ProductResponseModel itemData = await _itemsRepository.GetPresentItem().ConfigureAwait(false);
+                byte[] result = await _provider.WebClient.GetByteArrayAsync(itemData.ItemImage).ConfigureAwait(false);
 
-                _itemView.Title = itemData.Title;
-                _itemView.Currency = itemData.Currency;
-                _itemView.CurrencySign = itemData.CurrencySign;
-                byte[] result = await _provider.WebClient.GetByteArrayAsync(itemData.ItemImage);
-                //TODO: make async
+                await mainThreadTaskFactory.StartNew(delegate
                 {
-                    var tex = new Texture2D(0, 0, TextureFormat.RGB24, false);
-                    tex.LoadImage(result);
-                    _itemView.Icon = tex;
-                }
-               
+                    _itemView.Title = itemData.Title;
+                    _itemView.Currency = itemData.Currency;
+                    _itemView.CurrencySign = itemData.CurrencySign;
+                    _itemView.Icon = result;
+                }).ConfigureAwait(false);
             }
             catch (Exception e)
             {
